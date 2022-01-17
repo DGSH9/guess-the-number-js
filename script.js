96cf1adabc380118c934d380b3584a1cf4fa5a8c
Durgesh Kumar Yadav <57389386+DGSH9@users.noreply.github.com>
2022-01-17 19:18:13 +0530

    Guess the Number project completed

--- script.js
+++ script.js
@@ -5,15 +5,20 @@
 * DONE: compare user input with random number   = playGame()        (correctNumber with inputNumber)
 * DONE: compare user input/random Numner in fun = displayresult()   (create function to comare )
 * DONE: show result on browser                  = Dialog(), guessRight(), guessHigh(), guessLow()
-
+* DONE: call function in displayresult
+* DONE: iterate array and display
+* DONE: restart           
 
 **/
 
+// creating empty array to store all the user input
+let arr1 = []
 //global store rando num in correctNumber
 let correctNumber = getRandomNumber();
 
 window.onload = function () {
     document.getElementById("number-submit").addEventListener("click", playGame);
+    document.getElementById("restart-game").addEventListener("click", restartGame);
 }
 
 
@@ -21,19 +26,23 @@ window.onload = function () {
 function playGame() {
     let inputNumber = document.getElementById('number-guess').value;
     displayresult(inputNumber);
+    arr1History(inputNumber);
+    displayHistory()
 }
 
 // create function Too high, Too Low, Correct
 function displayresult(inputNumber) {
     if (correctNumber == inputNumber) {
-        console.log("correct");
+        // console.log("correct");
+        guessRight();
     }
     else if (inputNumber < correctNumber) {
-        console.log("Too Low");
+        // console.log("Too Low");
+        guessLow()
     }
-
     else {
-        console.log("Too High");
+        // console.log("Too High");
+        guessHigh();
     }
 }
 
@@ -44,7 +53,6 @@ function getRandomNumber() {
     let wholeNum = Math.floor(x * 100) + 1;
     return wholeNum;
 }
-
 
 
 
@@ -87,7 +95,37 @@ function guessLow() {
     document.getElementById("result").innerHTML = dialog;
 }
 
+// storing value in array for history
+function arr1History(inputNumber) {
+    arr1.push(inputNumber)
+}
 
 
+// display history
+function displayHistory() {
+    let i = arr1.length - 1;
+    let list = "<ul class='list-group'>";
+
+    while (i >= 0) {
+        list += "<li class='list-group-item'>" +
+            "You Enter : " + arr1[i] + "</li>";
+        i -= 1;
+    }
+
+    list += '</ul>';
+    document.getElementById("history").innerHTML = list;
+}
 
 
+// restart the game
+function restartGame() {
+    // reset the correct number(randome number)
+    // reset result display
+    // reset arr1
+    // reset history
+    correctNumber = getRandomNumber();
+    document.getElementById("result").innerHTML = "";
+    arr1 = [];
+    displayHistory();
+
+}
